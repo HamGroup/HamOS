c3c4188857e0b13c724fbaf9c850eb3cf0e800b4
Tehran motors <Tehran.motors.tm@gmail.com>
2025-02-16 18:23:54 +0330

    Update V0.2

--- HamOS/Main.cpp
+++ HamOS/Main.cpp
@@ -5,19 +5,26 @@
 #include <unordered_map>
 #include <windows.h>
 #include <conio.h>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 // Global variables
 unordered_map<char, int> variables;
+unordered_map<string, string> stringVariables;
 
 // Function prototypes
 void loadPrograms(vector<string>& programs);
 void runProgram(const string& filename);
 void displayStartupScreen(const vector<string>& programs, int currentSelection);
 void createVar(char VarName, int Value);
+void createStringVar(const string& VarName, const string& Value);
 void printVar(char VarName);
 void printStr(const string& str);
+void printStrVar(const string& VarName);
+void wait(int seconds);
+void printMulti(const vector<string>& lines);
 void executeLine(const string& line);
 
 int main() {
@@ -81,10 +88,9 @@ void displayStartupScreen(const vector<string>& programs, int currentSelection) 
     cout << "*               |_|  |_||__,_|_| |_| |_||____/|_____/           *" << endl;
     cout << "*                                                               *" << endl;
     cout << "*                                                               *" << endl;
-    cout << "*                                                               *" << endl;
     cout << "*****************************************************************" << endl;
     cout << "*                                                               *" << endl;
-    cout << "* V0.1                                            Legacy Edition*" << endl;
+    cout << "* V0.2                                        Developer Edition *" << endl;
     cout << "*****************************************************************" << endl;
     cout << endl;
     cout << "Available Programs:" << endl;
@@ -102,17 +108,32 @@ void displayStartupScreen(const vector<string>& programs, int currentSelection) 
 void runProgram(const string& filename) {
     ifstream file("programs/" + filename);
     string line;
+    vector<string> multiLine;
 
     while (getline(file, line)) {
-        executeLine(line);
+        if (line.find("printMulti {") == 0) {
+            while (getline(file, line) && line.find("}") == string::npos) {
+                multiLine.push_back(line);
+            }
+            printMulti(multiLine);
+            multiLine.clear();
+        }
+        else {
+            executeLine(line);
+        }
     }
 }
 
 void executeLine(const string& line) {
-    if (line.find("==") != string::npos) {
+    if (line.find("==") != string::npos && line[1] == ' ') {
         char varName = line[0];
         int value = stoi(line.substr(line.find("==") + 3));
         createVar(varName, value);
+    }
+    else if (line.find("==") != string::npos && line[1] != ' ') {
+        string varName = line.substr(0, line.find("==") - 1);
+        string value = line.substr(line.find("==") + 3);
+        createStringVar(varName, value);
     }
     else if (line.find("printVar") == 0) {
         char var = line[9];
@@ -122,10 +143,22 @@ void executeLine(const string& line) {
         string str = line.substr(10, line.size() - 11); // Extract string
         printStr(str);
     }
+    else if (line.find("printStrVar") == 0) {
+        string varName = line.substr(12, line.size() - 13); // Extract variable name
+        printStrVar(varName);
+    }
+    else if (line.find("wait") == 0) {
+        int seconds = stoi(line.substr(5, line.size() - 6)); // Extract seconds
+        wait(seconds);
+    }
 }
 
 void createVar(char VarName, int Value) {
     variables[VarName] = Value;
+}
+
+void createStringVar(const string& VarName, const string& Value) {
+    stringVariables[VarName] = Value;
 }
 
 void printVar(char VarName) {
@@ -139,4 +172,23 @@ void printVar(char VarName) {
 
 void printStr(const string& str) {
     cout << str << endl;
+}
+
+void printStrVar(const string& VarName) {
+    if (stringVariables.find(VarName) != stringVariables.end()) {
+        cout << stringVariables[VarName] << endl;
+    }
+    else {
+        cout << "String variable " << VarName << " not defined." << endl;
+    }
+}
+
+void wait(int seconds) {
+    this_thread::sleep_for(chrono::seconds(seconds));
+}
+
+void printMulti(const vector<string>& lines) {
+    for (const string& line : lines) {
+        cout << line << endl;
+    }
 }
